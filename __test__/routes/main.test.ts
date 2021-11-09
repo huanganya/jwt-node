ac272d8e8a175e58aebc7b3376398f78c011fa69
Anya Huang <huang.anya@activate.sg>
2021-11-09 20:03:48 +0800

    add express-validator; refactor the main.test.ts

---
+++ __test__/routes/main.test.ts
@@ -0,0 +1,9 @@
+describe("main router", () => {
+  describe("get /someendpoint", () => {
+    afterEach(jest.restoreAllMocks);
+
+    it("should response with OK status", async () => {
+      //this is the test for someendpoint
+    });
+  });
+});
